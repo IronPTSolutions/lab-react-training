2e7ca85b73c4ec498a431b733b7759a6954165b1
Carlos del Prado <carlos.delprado@11paths.com>
2021-02-26 22:02:00 +0000

    feat: iteration 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import IdCard from './components/id-card/IdCard';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container py-5">
+      <h3 className="mb-4">Iteration 1</h3>
+      <IdCard
+        lastName='Doe'
+        firstName='John'
+        gender='male'
+        height={178}
+        birth={new Date("1992-07-14")}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+        className="mb-2"
+      />
+      <IdCard
+        lastName='Delores '
+        firstName='Obrien'
+        gender='female'
+        height={172}
+        birth={new Date("1988-05-11")}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+        className="mb-2"
+      />
     </div>
   );
 }
---
+++ src/components/id-card/IdCard.js
@@ -0,0 +1,22 @@
+import './IdCard.css';
+
+function IdCard({ lastName, firstName, gender, height, birth, picture, className}) {
+  return (
+    <div className={`IdCard border p-1 ${className}`}>
+      <div className="d-flex">
+        <img src={picture} alt={firstName} />
+        <div className="contact-info">
+          <ul>
+            <li><strong>First name:</strong> {firstName}</li>
+            <li><strong>Last name:</strong> {lastName}</li>
+            <li><strong>Gender:</strong> {gender}</li>
+            <li><strong>Height:</strong> {height}m</li>
+            <li><strong>Birth:</strong> {birth.toDateString()}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default IdCard;
